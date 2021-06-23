c4361322c7d9c1d89a2cae0ae31aa9398b3b6e9b
Gabriel Jurez <empirenerd1138@gmail.com>
2021-06-22 20:12:52 -0700

    trying to get the first bcrty to work
    Havent test the bcrty yet

--- shopping-cart-store-Completed/backend-Node-Koa-API/index.js
+++ shopping-cart-store-Completed/backend-Node-Koa-API/index.js
@@ -9,14 +9,11 @@ const logger = require('koa-logger');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const json = require('koa-json')
+const bcrypt = require('bcrypt');
 app.use(logger());
 
 //const mysql = require('koa-mysql');
 const mysql = require('mysql2/promise');
-
-
-
-
 
 const port = process.env.PORT;
 const pool = mysql.createPool({
@@ -25,8 +22,6 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
-
-
 
 app.use(async function mysqlConnection(ctx, next) {
     try {
@@ -77,16 +72,77 @@ router.get('/products/:productCode', async ctx => {
 })
 
 
-router.get('/shipping' , async ctx =>{ 
+router.get('/shipping', async ctx => {
     try {
         const [shippingOptions] = await ctx.state.db.query(
             `SELECT * FROM shippingcost `
-        ); 
-            ctx.body = {shippingOptions}
-    } catch (err){ 
+        );
+        ctx.body = { shippingOptions }
+    } catch (err) {
         console.log('Error in the shipping Get', err)
     }
 })
+
+
+router.post('/registration', async ctx => {
+    try {
+        let customerreg;
+
+        // Hashes the password and inserts the info into the `user` table
+        await bcrypt.hash(ctx.body.passWord, 10).then(async hash => {
+            try {
+                [customerreg] = await ctx.state.db.query(`
+                INSERT INTO customerreg( customerLastName, customerFirstName, phone, 
+                    addressLine1, addressLine2, city, state, postalCode, userName, 
+                    email, passWord)
+                    VALUES (:customerLastName, :customerFirstName, :phone, 
+                        :addressLine1, :addressLine2, :city, :state, :postalCode, :userName, 
+                        :email, :passWord)
+                `, {
+                    customerLastName: ctx.body.customerLastName,
+                    customerFirstName: ctx.body.customerLastName,
+                    phone: ctx.body.phone,
+                    addressLine1: ctx.body.addressLine1,
+                    addressLine2: ctx.body.addressLine2,
+                    city: ctx.body.city,
+                    state: ctx.body.state,
+                    postalCode: ctx.body.postalCode,
+                    userName: ctx.body.userName,
+                    email: ctx.body.email,
+                    passWord: hash,
+                });
+                console.log('customerreg', customerreg)
+            } catch (error) {
+                res.json('Error creating customer');
+                console.log('error', error)
+            }
+        });
+        const encodedCustomer = jwt.sign(
+            {
+                customerID: customerreg.customerID,
+                ...ctx.body
+            },
+            process.env.JWT_KEY
+        );
+        ctx.json({
+            data: encodedCustomer,
+            error: false,
+            msg: ''
+        });
+    } catch (err) {
+        res.json({
+            data: null,
+            error: true,
+            msg: 'Error, please try again'
+        });
+        console.log('err', err)
+    }
+}) 
+    
+
+
+
+
 
 
 app.use(router.routes());
